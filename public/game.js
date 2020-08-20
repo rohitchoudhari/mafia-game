8c94de6819537afcbaab35b84b0ac7a0b5f8a9fe
Rohit Choudhari <rohit.choudhari1@gmail.com>
2020-08-20 05:36:00 +0530

    heroku deploy changes

--- public/game.js
+++ public/game.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://192.168.29.138:3000');
+var socket = io.connect('https://rtb-mafia-game.herokuapp.com/');
 
 // Query DOM
 var roomVal = document.getElementById('roomTxt');
